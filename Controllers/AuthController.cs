cfec500add4c3a0032ffcdffcd223d72983408f6
Luca Bercioux <Lucabex@gmail.com>
2026-08-24 16:03:55 +0100

    Add swagger

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using auth.Models;
+using auth.Data;
+using auth.DTOs;
+using Microsoft.EntityFrameworkCore;
+using BCrypt.Net;
+namespace auth.Controllers;
 
 
 [ApiController]
@@ -7,10 +12,32 @@ using auth.Models;
 public class AuthUser:ControllerBase
 {
     private readonly IHttpClientFactory _IClient;
+    private readonly AppDbContext _context;
 
-    public AuthUser(IHttpClientFactory iclientFactory)
+    public AuthUser(IHttpClientFactory iclientFactory,AppDbContext context)
     {
         _IClient = iclientFactory;
+        _context = context;
+    }  
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUser dto)
+    {
+        if(await _context.Users.AnyAsync(u=>u.UserName.ToLower() == dto.Name.ToLower()))
+        {
+            return BadRequest("User Name already in use");
+        }
+        var user = new User
+        {
+            UserName=dto.Name,
+            HashedPassword = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Ok("user Registered");
     }
+
+
     
 }
