9f027e3f937a5a0e9557cca8a76b35733a032db7
Luca Bercioux <Lucabex@gmail.com>
2026-09-05 20:28:58 +0100

    add authentication and authorization in program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using auth.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +11,29 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var JwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = JwtSettings["SecretKey"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme =JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime=true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = JwtSettings["Audience"],
+        ValidIssuer = JwtSettings["Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
+    };
+});
 builder.Services.AddDbContext<AppDbContext>(option =>
 option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
@@ -20,6 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
 app.UseSwaggerUI();
 }
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
